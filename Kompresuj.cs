284a31aac5de247e30e8c05ff4264a7f3abdd8ea
7akacz7 <andrzej.klimalagit@gmail.com>
2023-04-21 19:43:04 +0200

    Kompresuj

---
+++ Kompresuj.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class Kompresuj
+{
+    public static Pudelko Kompresuj(this Pudelko p)
+    {
+        var objetosc = p.Objetosc;
+        var krawedz = Math.Pow(objetosc, 1.0 / 3.0);
+        return new Pudelko(krawedz, krawedz, krawedz, UnitOfMeasure.meter);
+    }
+}
